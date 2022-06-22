0a1b3a88ff609e100fb1386bfa0357adc3a205f8
Gsr213 <steverivera118@gmail.com>
2022-06-21 21:09:34 -0400

    Adding structure to the files/folders

---
+++ Recommender.py
@@ -0,0 +1,5 @@
+
+
+
+
+print("This program will recommend to the user, public companies to invest in from the S&P500!")
---
+++ StockRecommender.py
@@ -0,0 +1,22 @@
+
+#The program will ask the user their preference for a series of categories
+#Recommendation categories: Industry, Country/State, Time on S&P, Time in existence, Stock price
+
+#Do you have a preference for Industry? Yes/No
+#If yes, show different industries as options, multiple selections allowed.
+
+#Do you have a preference for Country/State that the business is headquartered? Yes/No
+#If yes, show different countries/states as options, multiple selections allowed
+
+#Do you have a preference for how long the business has been part of S&P500? Yes/No
+#If yes, show different time frames (0-5, 5-10, 10-20, 20+), multiple selections allowed
+
+#Do you have a preference for how long the business has been in existence? Yes/No
+#If yes, show different time frames (0-5, 5-10, 10-20, 20+), multiple selections allowed
+
+#Do you have a preference for the price of the stock? Yes/No
+#If yes, specify a whole number dollar amount and only show stocks with price under that amount
+
+
+#The program recommends stocks to invest in 
+print("This program will recommend to the user, public companies to invest in from the S&P500!")
